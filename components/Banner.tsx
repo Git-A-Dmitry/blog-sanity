6d53e55c90d5e3cfedf96795a032b2fed4503bc5
Dmitry A <adcntt@gmail.com>
2023-07-16 19:37:59 +0300

    color changed

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -2,7 +2,7 @@ export default function Banner() {
   return (
     <div className='flex flex-col lg:flex-row lg:space-x-5 justify-between font-bold px-10 py-5 mb-10bonga'>
       <div>
-        <h1 className='text-6xl text-slate-700'>Dmitry&lsquo;s Blog</h1>
+        <h1 className='text-6xl text-slate-500'>Dmitry&lsquo;s Blog</h1>
         <h2 className='mt-5 md:mt-0'>{/* For <span className='decoration-4'>every developer</span> */}</h2>
       </div>
 
